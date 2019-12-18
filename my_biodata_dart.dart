105125c15dc933757f04106b81fb49b94e0e9288
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2019-12-18 08:32:04 +0700

    Create file my_biodata_dart.dart

---
+++ my_biodata_dart.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+main() {
+  String name = "Erik Rio Setiawan";
+  String address = "RT 01 RW 01 Dukuh Bibis, Desa Pager, Kecamatan Bungkal, Ponorogo";
+  int age = 22;
+
+  stdout.write('My name is $name. I\'m $age years old. And my address in $address');
+}
